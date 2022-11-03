80c7842737afc1a5f6c91afed33acdc3c87ae08f
Felipe Harlen Santos de Sousa <felipeharlen@hotmail.com>
2022-11-03 14:28:07 -0300

    Novo push 03-11-2022 14h28

---
+++ CursoWebModerno/ESNext/revisao_1.js
@@ -0,0 +1,12 @@
+// let e const
+{
+    var a = 2
+    let b = 3
+
+    console.log(b)
+}
+console.log(a)
+
+// Destructuring
+const [l, e, ...tras] = "Cod3r"
+console.log(l, e, tras)
---
+++ CursoWebModerno/ESNext/revisao_3.js
@@ -0,0 +1,24 @@
+// ESB: Object.values/Object.entries
+const obj = {a: 1, b:2, c: 3}
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
+
+// Notação literal
+const nome = 'Carla'
+const pessoa = {
+    nome, ola(){
+        return 'Oi, gente!'
+    }
+}
+
+console.log(pessoa.nome, pessoa.ola())
+
+//
+class Animal {}
+class Cachorro extends Animal {
+    falar(){
+        return 'Au au!'
+    }
+}
+
+console.log(new Cachorro().falar())
